4c59c8a4ae0b72ca7b7f685f2500c1268fed0378
Chris Mann <chrispmann@gmail.com>
2022-09-19 16:40:59 -0700

    got it mostly the same as lecture 1

---
+++ 01_components_and_props/project_showcase/src/components/ProjectForm.js
@@ -0,0 +1,9 @@
+const ProjectForm = () => {
+    return (
+        <form className="form">
+            <h2>Add Project</h2>
+        </form>
+    )
+}
+
+export default ProjectForm;
---
+++ 01_components_and_props/project_showcase/src/components/ProjectListItem.js
@@ -0,0 +1,21 @@
+const ProjectListItem = ({ project }) => {
+    console.log(project);
+    return (
+        <li className='card'>
+            <figure className='image'>
+                <img src={project.image} alt={`${project.name} app`} />
+            </figure>
+            <section className='details'>
+                <h4>{project.name}</h4>
+                <p>{project.about}</p>
+                <p><a href={project.link}>Link</a></p>
+            </section>
+            <footer className='extra'>
+                <span className='badge blue'>Phase {project.phase}</span>
+            </footer>
+        </li>
+    );
+
+}
+
+export default ProjectListItem;
